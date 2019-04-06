d00585eb2a44fac57630c4cd04ec94a32a939f46
Văn Toản Trần <tranvantoan@ginno.local>
2019-04-06 11:19:42 +0700

    code load hex file

--- STM32LoadCode/Form1.Designer.cs
+++ STM32LoadCode/Form1.Designer.cs
@@ -29,12 +29,131 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.textBoxHexPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxBaudrate = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonLoadCode = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonConnectCOM = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxHexPath
+            // 
+            this.textBoxHexPath.Location = new System.Drawing.Point(24, 39);
+            this.textBoxHexPath.Name = "textBoxHexPath";
+            this.textBoxHexPath.Size = new System.Drawing.Size(459, 20);
+            this.textBoxHexPath.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "HEX file";
+            // 
+            // textBoxBaudrate
+            // 
+            this.textBoxBaudrate.Location = new System.Drawing.Point(87, 8);
+            this.textBoxBaudrate.Name = "textBoxBaudrate";
+            this.textBoxBaudrate.Size = new System.Drawing.Size(82, 20);
+            this.textBoxBaudrate.TabIndex = 1;
+            this.textBoxBaudrate.Text = "9600";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Baud Rate";
+            // 
+            // buttonLoadCode
+            // 
+            this.buttonLoadCode.Location = new System.Drawing.Point(24, 228);
+            this.buttonLoadCode.Name = "buttonLoadCode";
+            this.buttonLoadCode.Size = new System.Drawing.Size(459, 59);
+            this.buttonLoadCode.TabIndex = 4;
+            this.buttonLoadCode.Text = "Load HEX file to STM32";
+            this.buttonLoadCode.UseVisualStyleBackColor = true;
+            this.buttonLoadCode.Click += new System.EventHandler(this.buttonLoadCode_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonConnectCOM);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBoxBaudrate);
+            this.panel1.Location = new System.Drawing.Point(24, 78);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 105);
+            this.panel1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "COM";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(87, 40);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(82, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // buttonConnectCOM
+            // 
+            this.buttonConnectCOM.Location = new System.Drawing.Point(26, 67);
+            this.buttonConnectCOM.Name = "buttonConnectCOM";
+            this.buttonConnectCOM.Size = new System.Drawing.Size(143, 30);
+            this.buttonConnectCOM.TabIndex = 7;
+            this.buttonConnectCOM.Text = "Connect";
+            this.buttonConnectCOM.UseVisualStyleBackColor = true;
+            this.buttonConnectCOM.Click += new System.EventHandler(this.buttonConnectCOM_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(502, 310);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonLoadCode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxHexPath);
+            this.Name = "Form1";
+            this.Text = "Mr. Dai Du";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxHexPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxBaudrate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonLoadCode;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonConnectCOM;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- STM32LoadCode/Form1.cs
+++ STM32LoadCode/Form1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,78 @@ namespace STM32LoadCode
         public Form1()
         {
             InitializeComponent();
+
+            string[] allSerialComs = SerialPort.GetPortNames();
+            comboBox1.DataSource = allSerialComs;
+
+            comboBox1.SelectedItem = allSerialComs[0];
+        }
+
+        private void buttonLoadCode_Click(object sender, EventArgs e)
+        {
+
+            if(!File.Exists(textBoxHexPath.Text))
+            {
+                MessageBox.Show("Path not existed: " + textBoxHexPath.Text);
+                return;
+            }
+
+            if (!textBoxHexPath.Text.ToUpper().Contains(".HEX"))
+            {
+                MessageBox.Show("Not a hex file!");
+                return;
+            }
+
+            if (!serialPort1.IsOpen)
+            {
+                //serialPort1.Close();
+                MessageBox.Show("Serial Port is not open!");
+                return;
+            }
+
+            byte[] hexBin = File.ReadAllBytes(textBoxHexPath.Text);
+            serialPort1.Write(hexBin, 0, hexBin.Length);
+        }
+
+        private void buttonConnectCOM_Click(object sender, EventArgs e)
+        {
+            if(!textBoxBaudrate.Enabled)
+            {
+                if(serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                }
+
+                textBoxBaudrate.Enabled = true;
+                comboBox1.Enabled = true;
+                buttonConnectCOM.Text = "Connect";
+            }
+            else
+            {
+                int baudRate;
+                bool parse = int.TryParse(textBoxBaudrate.Text, out baudRate);
+                if (!parse)
+                {
+                    MessageBox.Show("Baudrate error!");
+                    return;
+                }
+
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                }
+
+                serialPort1.PortName = comboBox1.Text;
+                serialPort1.BaudRate = baudRate;
+
+                serialPort1.Open();
+
+                textBoxBaudrate.Enabled = false;
+                comboBox1.Enabled = false;
+
+                buttonConnectCOM.Text = "Cancel";
+            }
+        
         }
     }
 }
